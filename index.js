590fa3edbe8956f23601050f5dcb18616ed94877
yaoleshchuk <yoleshchuk@upteam.com>
2023-04-04 23:25:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
-#!/usr/bin/env node
-import Mustache from 'mustache';
-import fs from 'fs';
-import axios from 'axios';
-
+const Mustache = require('mustache');
+const fs = require('fs');
+const axios = require('axios');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY || '';
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
 
-interface IData {
-  name: string;
-  date: string;
-  weather?: {
-    temp: string;
-    description: string;
-  }
-}
+let DATA = {
+  name: 'Yaroslav',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Europe/Paris',
+  }),
+};
 
-const generateReadMe = () => {
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Cannes&units=metric&appid=${OPENWEATHER_API_KEY}`)
+function generateReadMe() {
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Cannes&units=metric&appid=${OPENWEATHER_API_KEY}`
+    )
     .then((res) => {
       const weather = {
         temp: res.data.main.temp.toFixed(1),
         description: res.data.weather[0].description,
       };
-      const DATA: IData = {
-        name: 'Yaroslav',
-        date: new Date().toLocaleDateString('en-GB', {
-          weekday: 'long',
-          month: 'long',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-          timeZoneName: 'short',
-          timeZone: 'Europe/Paris',
-        }),
-        weather,
-      };
+      DATA.weather = weather;
       fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
         if (err) throw err;
         const output = Mustache.render(data.toString(), DATA);
@@ -44,6 +37,6 @@ const generateReadMe = () => {
     .catch((err) => {
       console.error(err);
     });
-};
+}
 
 generateReadMe();
