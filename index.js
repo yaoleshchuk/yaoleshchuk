8dd2e6a570094e1532ecd9092b7ca4dba8d0d4d1
yaoleshchuk <yoleshchuk@upteam.com>
2023-04-04 21:05:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,34 @@
-// index.ts
-import * as Mustache from 'mustache';
-import * as fs from 'fs';
-import axios from 'axios';
-
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-
-interface WeatherData {
-  name: string;
-  description: string;
-  temp: number;
-  feelsLike: number;
-}
-
-interface MustacheData {
-  name: string;
-  date: string;
-  weather: WeatherData;
-}
-
-const generateReadMe = async (): Promise<void> => {
-  const date = new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'Europe/Paris',
-  });
-
-  const weather = await getWeather();
-
-  const data: MustacheData = {
+/**
+ * DATA is the object that contains all
+ * the data to be provided to Mustache
+ * Notice the "name" and "date" property.
+ */
+let DATA = {
     name: 'Yaroslav',
-    date: date,
-    weather: weather,
-  };
-
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, file) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    const output = Mustache.render(file.toString(), data);
-    fs.writeFileSync('README.md', output);
-  });
+    date: new Date().toLocaleDateString('en-GB', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'short',
+        timeZone: 'Europe/Paris',
+    }),
 };
-
-const getWeather = async (): Promise<WeatherData> => {
-  const city = 'Cannes';
-  const apiKey = 'dc966d561328929c144b72940a6ab7e9';
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  try {
-    const response = await axios.get(url);
-    const { name, weather, main } = response.data;
-    const weatherData: WeatherData = {
-      name,
-      description: weather[0].description,
-      temp: main.temp,
-      feelsLike: main.feels_like,
-    };
-    return weatherData;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
+/**
+ * A - We open 'main.mustache'
+ * B - We ask Mustache to render our file with the data
+ * C - We create a README.md file with the generated output
+ */
+function generateReadMe() {
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), DATA);
+        fs.writeFileSync('README.md', output);
+    });
+}
 generateReadMe();
